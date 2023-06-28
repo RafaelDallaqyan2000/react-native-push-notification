b557bcdc2506ae034d28c97b166259ecc2a41456
Rafael <rafaeldallakyan4@gmail.com>
2023-06-28 11:28:17 +0400

    s

---
+++ App.tsx
@@ -0,0 +1,70 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  Dimensions,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View
+} from "react-native";
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+type SectionProps = PropsWithChildren<{
+  title: string;
+}>;
+
+function Section({children, title}: SectionProps): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View>
+      <Text
+        style={[
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+}
+
+function App(): JSX.Element {
+  return <SafeAreaView style={styles.screen}></SafeAreaView>;
+}
+
+const window = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+  screen: {
+    backgroundColor: '#FFF',
+    height: window.height,
+  }
+});
+
+export default App;
